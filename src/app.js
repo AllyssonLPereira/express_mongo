9d3faa0851b66a38d011b598400241534f858c0d
Allysson <allyssonlunguinho.03i@gmail.com>
2025-05-23 17:02:51 -0300

    criando uma nova rota para 'livros', com uma base de dados no proprio codigo

--- src/app.js
+++ src/app.js
@@ -2,8 +2,23 @@ import express from "express";
 
 const app = express();
 
+const livros = [
+    {
+        id: 1,
+        titulo: "Black Hat Python - 2° Edição",
+    },
+    {
+        id: 2,
+        titulo: "Python Poderoso",
+    }
+]
+
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js")
 });
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros)
+})
 
 export default app;
