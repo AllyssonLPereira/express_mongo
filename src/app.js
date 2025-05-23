af4ba6ca70d027af9baee1baf6f1b19ab33837bb
Allysson <allyssonlunguinho.03i@gmail.com>
2025-05-23 18:05:18 -0300

    criando nova rota para acessar um unico livro por id

--- src/app.js
+++ src/app.js
@@ -14,12 +14,23 @@ const livros = [
     }
 ];
 
+function buscaLivros(id) {
+    return livros.findIndex(livro => {
+        return livro.id === Number(id);
+    });
+};
+
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js");
 });
 
 app.get("/livros", (req, res) => {
     res.status(200).json(livros);
+});
+
+app.get("/livros/:id", (req, res) => {
+    const index = buscaLivros(req.params.id);
+    res.status(200).json(livros[index]);
 });
 
 app.post("/livros", (req, res) => {
